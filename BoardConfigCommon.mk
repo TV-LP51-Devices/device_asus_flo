1182528e972503a42b85b2dbb9d122373552772c
Randy DuCharme <radio.ad5gb@gmail.com>
2015-04-19 16:12:49 -0500

    Fix bootani settings, try 4.9 toolchain for kernel
    Signed-off-by: Randy DuCharme <radio.ad5gb@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,6 +35,8 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 TARGET_KERNEL_SOURCE := kernel/google/msm
 TARGET_KERNEL_CONFIG := cyanogen_flo_defconfig
 
+ARM_EABI_TOOLCHAIN= := prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/bin
+
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
 BOARD_USES_FLUENCE_INCALL := true
